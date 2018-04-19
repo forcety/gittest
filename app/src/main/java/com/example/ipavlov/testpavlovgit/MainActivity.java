e417e2efd18013c0fc47e86bb3dc4e830e27624d
forcety <forcety@mail.ru>
2018-04-19 14:34:57 +0300

    Ilya 1 modified some cool code

--- app/src/main/java/com/example/ipavlov/testpavlovgit/MainActivity.java
+++ app/src/main/java/com/example/ipavlov/testpavlovgit/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Some cool code here ...
+        // Ilya 1 modified some cool code here ...
 
     }
 }
